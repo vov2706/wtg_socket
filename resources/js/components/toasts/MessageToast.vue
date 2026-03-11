fcebd868637c060febad20c12513b4916cd74b46
vyevsiuk <emexesemexes@gmail.com>
2026-03-12 00:05:42 +0200

    add little hint on toast

--- resources/js/components/toasts/MessageToast.vue
+++ resources/js/components/toasts/MessageToast.vue
@@ -6,14 +6,18 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-start gap-3">
-    <div class="flex flex-col">
-      <span class="text-sm font-semibold text-white">
+  <div class="flex cursor-pointer items-start gap-3">
+    <div class="flex min-w-0 flex-col">
+      <span class="text-sm leading-tight font-semibold break-words text-white">
         {{ sender }}
       </span>
 
-      <span class="text-xs text-white/80">
+      <span class="text-xs break-words whitespace-normal text-white/80">
         {{ message }}
+      </span>
+
+      <span class="mt-1 text-[11px] text-white/60">
+        Click or close to mark as read
       </span>
     </div>
   </div>
